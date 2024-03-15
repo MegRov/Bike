9fc486217c388f9f04d5649281e85a9cc5dbd43e
Miguel <47696065+MegRov@users.noreply.github.com>
2024-03-15 12:20:34 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php include 'modulos/head.php'?>
+</head>
+
+<body>
+    <!-- Preloader Start -->
+    <div id="preloader">
+        <div class="david-load"></div>
+    </div>
+
+
+   <!-- Navbar -->
+   <nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">
+      <img src="img/logo.png" width="200" height="50" class="d-inline-block align-top" alt="">
+      
+    </a>
+  </nav>
+ 
+  
+  <section class=" bg-secondary">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 mx-auto">
+        <h1 class="text-center text-white">¿Que hacemos?</h1>
+        <blockquote class="text-center text-white">Envíos a toda Cundinamarca, el precio es $2.000 cop por kilometro
+          
+        </blockquote>             
+
+
+
+
+        </div><!----end class------>
+      </div><!----end row------>
+    </div> <!----end container------>
+
+    
+    
+      <section class="bg-dark pl-5">                                                                
+        <div class="bg-dark text-center">
+           <div class="row">
+          
+              <div class="row col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+               
+                <div class="p-4" >
+                <div class="card" style="width: 18rem;" >
+                  <img  src="img/peque.jpg" class="card-img-top" >
+                  <div class="card-body">
+                    <h5 class="card-title">Entrega ligera</h5>
+                    <p class="card-text">Paquetes menores a 5kg. $2.000(cop)/km</p>
+                  
+                    <a  href="ligera.php" class="btn btn-secondary btn-lg btn-block"  role="button" aria-pressed="true">Saber más</a>
+                    
+                  </div>
+                </div>
+              </div>
+           
+              
+              
+              <div class="p-4">
+                <div class="card" style="width: 18rem;">
+                  <img src="img/medio.JPG" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Entrega pesada</h5>
+                    <p class="card-text">Paquete mayor a 10kg, $500(cop)/kilo .</p>
+                    <a  href="pesada.php" class="btn btn-secondary btn-lg btn-block" role="button" aria-pressed="true">Saber más</a>
+
+                  </div>
+                </div>
+              </div>
+            
+              
+              <div class="p-4">
+                <div class="card" style="width: 18rem;">
+                  <img src="img/programado.JPG" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Entrega programada</h5>
+                    <p class="card-text">Si tienes una ruta habrá descuento por cada paquete, -200 x paquete</p>
+                    <a  href="programada.php" class="btn btn-secondary btn-lg btn-block" role="button" aria-pressed="true">Saber más</a>
+
+                  </div>
+                </div>
+              </div>
+             
+              
+              
+              <div class="p-4">
+                <div class="card" style="width: 18rem;">
+                  <img src="img/regalo.JPG" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Entrega regalos</h5>
+                    <p class="card-text">Si nos compras el regalo el envío es gratis.</p>
+                    <a  href="regalo.php" class="btn btn-secondary btn-lg btn-block" role="button" aria-pressed="true">Saber más</a>
+
+                  </div>
+                </div>
+               
+            
+             </div><!----end container------>
+            </div><!----end container------> 
+         </div><!----end container------>                           
+
+    <!-- ****** Footer Menu Area Start ****** -->
+    <?php include 'modulos/footer.php'?>
+
+    <!-- ****** Footer Menu Area End ****** -->
+
+    <!--********************* scripts********************* -->
+    <?php include 'modulos/scripts.php'?>
+    <!--*********************End scripts********************* -->
+</body>
---
+++ pesada.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<head>
+  <?php include 'modulos/head.php'?>
+</head>
+
+<body>
+    <!-- Preloader Start -->
+    <div id="preloader">
+        <div class="david-load"></div>
+    </div>
+
+
+   <!-- Navbar -->
+   <nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.php">
+      <img src="img/logo.png" width="200" height="50" class="d-inline-block align-top" alt="">
+      
+    </a>
+  </nav>
+ 
+  
+  <section class=" bg-secondary">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 mx-auto">
+        <h1 class="text-center text-white">Carga pesada</h1>
+        <blockquote class="text-center text-white">
+          
+        </blockquote>             
+
+
+
+
+        </div><!----end class------>
+      </div><!----end row------>
+    </div> <!----end container------>
+
+
+<div class="container  bg-light mb-4 rounded">
+    <div class="row">
+  <div class="col-4 pl-0">
+    <div class="list-group" id="list-tab" role="tablist">
+      <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">¿Que es una carga ligera?</a>
+      <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">¿Quien lo recoge?</a>
+      <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Garantía</a>
+      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Contacto</a>
+    </div>
+  </div>
+  <div class="col-8">
+    <div class="tab-content bg-light text-center" id="nav-tabContent">
+      <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">Ó carga express, pesa menos de 6kg. </div>
+      <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">Su encomienda es igual de importante para nosotros, por eso el bici mensajero que recoja su paquete tendrá el deber de mostrar la identificacion que lo acredita como bike express</div>
+      <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">-Su paquete será entregado en un plazo de 4 horas después de recogido, y si no es así ¡su envío será gratis!. <br>
+                                                                                                         -Si su paquete presenta daños de transporte ó por cualquier motivo no llega, será repuesto con dinero por el valor que corresponda. <br>
+                                                                                                         -Usted tiene derecho a saber la ubicación del mensajero que lleva su producto en tiempo real.
+                                                                                                         Para más información sobre nuestras politicas dar click <a href="index.php">acá</a> </div>
+      <div class="tab-pane fade bg-dark" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">
+
+      <div class="card text-white bg-dark mb-3 center" >
+  <div class="card-header text-center">Bike express</div>
+  <div class="card-body">
+    <h5 class="card-title text-center">Haz tu solicitud por</h5>
+    <p class="card-text text-center"> whatsapp</p>
+    <p class="card-text text-center">+57 312 492 8910</p>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+    <!-- ****** Welcome Post Area Start ****** -->
+    <section class="welcome-post-sliders owl-carousel">
+
+
+        <!-- Single Slide -->
+        <div class="welcome-single-slide">
+            <!-- Post Thumb -->
+            <img src="img/0.JPG" alt="">
+            <!-- Overlay Text -->
+            <div class="project_title">
+                <div class="post-date-commnents d-flex">
+                    <a href="#">May 19, 2017</a>
+                    <a href="#">5 Comment</a>
+                </div>
+                <a href="#">
+                    <h5></h5>
+                </a>
+            </div>
+        </div>
+
+        <!-- Single Slide -->
+        <div class="welcome-single-slide">
+            <!-- Post Thumb -->
+            <img src="../img/1.JPG" alt="">
+            <!-- Overlay Text -->
+            <div class="project_title">
+                <div class="post-date-commnents d-flex">
+                    <a href="#">Mar 18, 2018</a>
+                    <a href="#">8 Comment</a>
+                </div>
+                <a href="#">
+                    <h5></h5>
+                </a>
+            </div>
+        </div>
+
+        <!-- Single Slide -->
+        <div class="welcome-single-slide">
+            <!-- Post Thumb -->
+            <img src="img/2.JPG" alt="">
+            <!-- Overlay Text -->
+            <div class="project_title">
+                <div class="post-date-commnents d-flex">
+                    <a href="#">Jul 30, 2018</a>
+                    <a href="#">5 Comment</a>
+                </div>
+                <a href="#">
+                    <h5></h5>
+                </a>
+            </div>
+        </div>
+
+        <!-- Single Slide -->
+        <div class="welcome-single-slide">
+            <!-- Post Thumb -->
+            <img src="img/3.JPG" alt="">
+            <!-- Overlay Text -->
+            <div class="project_title">
+                <div class="post-date-commnents d-flex">
+                    <a href="#">Ago 10, 2018</a>
+                    <a href="#">5 Comment</a>
+                </div>
+                <a href="#">
+                    <h5></h5>
+                </a>
+            </div>
+        </div>
+
+        <!-- Single Slide -->
+        <div class="welcome-single-slide">
+            <!-- Post Thumb -->
+            <img src="img/4.JPG" alt="">
+            <!-- Overlay Text -->
+            <div class="project_title">
+                <div class="post-date-commnents d-flex">
+                    <a href="#">Ene 3, 2019</a>
+                    <a href="#">40 Comment</a>
+                </div>
+                <a href="#">
+                    <h5></h5>
+                </a>
+            </div>
+        </div>
+
+        <!-- Single Slide -->
+        <div class="welcome-single-slide">
+            <!-- Post Thumb -->
+            <img src="img/5.JPG" alt="">
+            <!-- Overlay Text -->
+            <div class="project_title">
+                <div class="post-date-commnents d-flex">
+                    <a href="#">Feb 15, 2019</a>
+                    <a href="#">5 Comment</a>
+                </div>
+                <a href="#">
+                    <h5></h5>
+                </a>
+            </div>
+        </div>
+
+        <!-- Single Slide -->
+        <div class="welcome-single-slide">
+            <!-- Post Thumb -->
+            <img src="img/6.JPG" alt="">
+            <!-- Overlay Text -->
+            <div class="project_title">
+                <div class="post-date-commnents d-flex">
+                    <a href="#">feb 19, 2019</a>
+                    <a href="#">5 Comment</a>
+                </div>
+                <a href="#">
+                    <h5></h5>
+                </a>
+            </div>
+        </div>
+        
+    <!-- Single Slide -->
+    <div class="welcome-single-slide">
+            <!-- Post Thumb -->
+            <img src="img/7.JPG" alt="">
+            <!-- Overlay Text -->
+            <div class="project_title">
+                <div class="post-date-commnents d-flex">
+                    <a href="#">feb 19, 2019</a>
+                    <a href="#">5 Comment</a>
+                </div>
+                <a href="#">
+                    <h5></h5>
+                </a>
+            </div>
+        </div>
+    <!-- Single Slide -->
+    <div class="welcome-single-slide">
+            <!-- Post Thumb -->
+            <img src="img/8.JPG" alt="">
+            <!-- Overlay Text -->
+            <div class="project_title">
+                <div class="post-date-commnents d-flex">
+                    <a href="#">feb 19, 2019</a>
+                    <a href="#">5 Comment</a>
+                </div>
+                <a href="#">
+                    <h5></h5>
+                </a>
+            </div>
+        </div>
+        
+
+    </section>
+    <!-- ****** Welcome Area End ****** -->
+            
+
+
+
+   
+         
+                        
+
+    <!-- ****** Footer Menu Area Start ****** -->
+    <?php include 'modulos/footer.php'?>
+
+    <!-- ****** Footer Menu Area End ****** -->
+
+    <!--********************* scripts********************* -->
+    <?php include 'modulos/scripts.php'?>
+    <!--*********************End scripts********************* -->
+</body>
